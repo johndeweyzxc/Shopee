29ece760433786d0bac04ac8204349b62f8d7be0
johndeweyzxc <johndewey02003@gmail.com>
2022-12-17 18:33:48 +0800

    Added a component for footer

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,7 +9,19 @@ import ShopeeBig from "./assets/images/shopee-big.jpg";
 import GoogleIcon from "./assets/images/google-icon.png";
 import FacebookIcon from "./assets/images/facebook-icon.png";
 
-function TopAppBar() {
+import FacebookBlack from "./assets/images/FollowUs/facebook-icon.png";
+import InstagramBlack from "./assets/images/FollowUs/instagram-icon.png";
+import LinkedInBlack from "./assets/images/FollowUs/linked-in-icon.png";
+import TwitterBlack from "./assets/images/FollowUs/twitter-icon.png";
+import Payments from "./assets/images/payments.jpg";
+import Logistics from "./assets/images/logistics.jpg";
+
+import QrCode from "./assets/images/ShopeeAppDL/qr-code.jpg";
+import AppStore from "./assets/images/ShopeeAppDL/app-store.jpg";
+import GooglePlay from "./assets/images/ShopeeAppDL/google-play.jpg";
+import AppGallery from "./assets/images/ShopeeAppDL/app-gallery.jpg";
+
+export function TopAppBar() {
   return (
     <header className='w-full bg-primaryBgColor p-4 pl-6 pr-6 flex justify-between ml-12'>
       <img src={ShopeeHeader} />
@@ -46,7 +58,7 @@ function AppBody() {
         </h2>
 
         <div className='flex flex-col w-80'>
-          <div className='mb-4'>
+          <div className='mb-4 font-Roboto'>
             <div className='mb-4'>
               <TextField variant='outlined' label='Username' sx={{width: 1}} />
             </div>
@@ -85,7 +97,7 @@ function AppBody() {
             </Button>
           </div>
 
-          <div className='flex justify-between mb-6'>
+          <div className='flex justify-between mb-6 font-Roboto'>
             <a href='#' className='text-sm text-blue-500'>
               Forgot Password
             </a>
@@ -94,7 +106,7 @@ function AppBody() {
             </a>
           </div>
 
-          <div className='flex items-center justify-evenly mb-6'>
+          <div className='flex items-center justify-evenly mb-6 font-Roboto'>
             <div className='bg-dividerColor h-[1px] w-full' />
             <div className='whitespace-nowrap mr-4 ml-4 text-secondaryTextColor'>
               Or login with
@@ -127,7 +139,7 @@ function AppBody() {
             </div>
           </div>
 
-          <div className='mb-4 flex justify-center items-center'>
+          <div className='flex justify-center items-center font-Roboto'>
             <div className='text-secondaryTextColor text-sm'>
               New to Shopee?{" "}
               <b className='text-primaryColor cursor-pointer text-sm'>
@@ -141,7 +153,14 @@ function AppBody() {
   );
 }
 
-function BottomBar() {
+export function BottomBar() {
+  const FollowUs = [
+    {name: "Facebook", img: FacebookBlack},
+    {name: "Instagram", img: InstagramBlack},
+    {name: "Twitter", img: TwitterBlack},
+    {name: "LinkedIn", img: LinkedInBlack},
+  ];
+
   const CustomerService = [
     "Help Centre",
     "Shopee Cares PH",
@@ -153,7 +172,7 @@ function BottomBar() {
     "Return & Refund",
     "Shopee Gurantee",
     "Overseas Product",
-    " Contact Us",
+    "Contact Us",
   ];
 
   const AboutShopee = [
@@ -169,34 +188,94 @@ function BottomBar() {
   ];
 
   return (
-    <div className='bg-primaryBgColor p-24'>
-      <div className='flex'>
-        <div className='mr-24'>
-          <h4 className='mb-4 font-bold'>CUSTOMER SERVICE</h4>
-          {CustomerService.map((data, i) => {
-            return (
-              <div className='mb-2 text-sm cursor-pointer hover:font-semibold'>
-                {data}
+    <div className='bg-primaryBgColor p-24 font-Roboto w-full'>
+      <div className='flex justify-between'>
+        <section className='flex'>
+          <div className='mr-24'>
+            <h4 className='mb-4'>CUSTOMER SERVICE</h4>
+            {CustomerService.map((data, i) => {
+              return (
+                <div
+                  className='mb-2 text-sm cursor-pointer text-secondaryTextColor 
+                hover:text-primaryColor'
+                >
+                  {data}
+                </div>
+              );
+            })}
+          </div>
+          <div className='mr-36'>
+            <h4 className='mb-4'>ABOUT SHOPEE</h4>
+            {AboutShopee.map((data, i) => {
+              return (
+                <div
+                  className='mb-2 text-sm cursor-pointer text-secondaryTextColor 
+                hover:text-primaryColor'
+                >
+                  {data}
+                </div>
+              );
+            })}
+          </div>
+          <div className='mr-16'>
+            <div className='mb-12'>
+              <h4 className='mb-4'>PAYMENT</h4>
+              <img src={Payments} />
+            </div>
+            <div>
+              <h4 className='mb-4'>LOGISTICS</h4>
+              <img src={Logistics} />
+            </div>
+          </div>
+        </section>
+
+        <section className='flex'>
+          <div className='mr-28'>
+            <h4 className='mb-4'>FOLLOW US</h4>
+            {FollowUs.map((data, i) => {
+              return (
+                <div
+                  className='text-sm text-secondaryTextColor flex mb-2 cursor-pointer
+                hover:text-primaryColor self-center'
+                >
+                  <img src={data.img} className='h-6 mr-2' />
+                  {data.name}
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <h4 className='mb-4'>SHOPEE APP DOWNLOAD</h4>
+            <div className='flex'>
+              <div className='mr-2'>
+                <img
+                  src={QrCode}
+                  className='border-[1px] border-dividerColor rounded-md shadow-sm cursor-pointer'
+                />
               </div>
-            );
-          })}
-        </div>
-        <div>
-          <h4 className='mb-4 font-bold'>ABOUT SHOPEE</h4>
-          {AboutShopee.map((data, i) => {
-            return (
-              <div className='mb-2 text-sm cursor-pointer hover:font-semibold'>
-                {data}
+              <div className='flex flex-col justify-between'>
+                <img
+                  src={AppStore}
+                  className='border-[1px] border-dividerColor rounded-md shadow-sm cursor-pointer'
+                />
+                <img
+                  src={GooglePlay}
+                  className='border-[1px] border-dividerColor rounded-md shadow-sm cursor-pointer'
+                />
+                <img
+                  src={AppGallery}
+                  className='border-[1px] border-dividerColor rounded-md shadow-sm cursor-pointer'
+                />
               </div>
-            );
-          })}
-        </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
 }
 
-export default function Login() {
+export function Login() {
   return (
     <div className='w-screen h-auto'>
       <TopAppBar />
